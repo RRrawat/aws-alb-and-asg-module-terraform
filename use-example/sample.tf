59e2bacd276210b4f83b5547ac05460a4c096a59
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-15 14:17:30 +0530

    Update sample.tf

--- use-example/sample.tf
+++ use-example/sample.tf
@@ -1,4 +1,17 @@
 module "alb_with_asg" {
   source = "../"
+  region = var.region
+  vpc_id = var.vpc_id 
+  create_lc = var.create_lc
+  instance_type = var.instance_type
+  key_name = var.key_name 
+  ingress = var.ingress
+  create_asg = var.create_asg
+  min_size = var.min_size
+  max_size = var.max_size
+  desired_capacity = var.desired_capacity 
+  wait_for_capacity_timeout = var.wait_for_capacity_timeout 
+  health_check_type = var.health_check_type
+  ingress_alb = var.ingress_alb =
   
 }
