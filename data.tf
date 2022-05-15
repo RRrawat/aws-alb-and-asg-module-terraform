81a7a2cac1ef9d7815c6cc2804d1d21f41bc96a0
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-15 14:07:31 +0530

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,34 @@
+data "aws_availability_zones" "all" {}
+
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+  owners = ["amazon"]
+  filter {
+    name = "owner-alias"
+
+    values = [
+      "amazon",
+    ]
+  }
+}
+
+data "aws_vpc" "vpc" {
+  id = var.vpc_id
+}
+data "aws_subnet_ids" "subnets" {
+  vpc_id = data.aws_vpc.vpc.id
+}
+
+
+module "tags" {
+  source = "./common_tags"
+
+  enviroment = var.enviroment
+  additional_tags = var.additional_tags
+  
+}
