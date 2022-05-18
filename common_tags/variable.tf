c1bb16cf6e340c687d72880259d643757f1b7db3
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-05-18 09:15:58 +0530

    Create variable.tf

---
+++ common_tags/variable.tf
@@ -0,0 +1,10 @@
+variable "enviroment" {
+    type = string
+	description	= "Provide environment (DEV/TEST/PRD)"
+}
+
+variable "additional_tags"{
+	type = map(string)
+	default = null
+	description = "If user wants to add any other additional tags to resource"
+}
