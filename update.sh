21ae00ce9043e633b87c86b0a3edce3522b09547
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-15 11:13:44 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,19 +1,13 @@
 #!/bin/bash -v
 
 sudo su -
-
 cd /opt
-
 amazon-linux-extras install java-openjdk11 -y
 wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.60/bin/apache-tomcat-9.0.60.tar.gz
 tar -xvzf /opt/apache-tomcat-9.0.60.tar.gz
 mv apache-tomcat-9.0.60 tomcat
-
 chown ec2-user /opt/tomcat/webapps
-
 ln -s /opt/tomcat/bin/startup.sh /usr/local/bin/tomcatup
 ln -s /opt/tomcat/bin/shutdown.sh /usr/local/bin/tomcatdown
-
 tomcatup
-
 cd ..
